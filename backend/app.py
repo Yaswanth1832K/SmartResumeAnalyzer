49c73c7db259c82817a662e3899e19c48322be0a
Yaswanth1832K <yaswanthjallipalli@gmail.com>
2026-02-09 12:52:48 +0530

    Update backend to use dynamic PORT environment variable

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 import os
 import re
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
@@ -189,4 +190,5 @@ def analyze_resume():
     return jsonify(response), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
